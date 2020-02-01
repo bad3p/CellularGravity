ffded60d5128e7a10c4bcc4aae4e2db3f641fc74
bad3p <sxl@list.ru>
2020-02-01 09:50:29 +0300

    F! In progress sub-cell mass & volume description with AABBs

---
+++ Assets/Scripts/Editor/RectTestEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(RectTest))]
+public class RectTestEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        RectTest rectTest = (RectTest) target;
+        if (rectTest == null)
+        {
+            return;
+        }
+
+        Vector4 rectTestBounds = rectTest.Bounds;
+        float xInf = rectTestBounds.x;
+        float yInf = rectTestBounds.y;
+        float xSup = rectTestBounds.z;
+        float ySup = rectTestBounds.w;
+        
+        Handles.color = Color.white;
+        Handles.Label
+        (
+            new Vector3(xInf, 0, yInf),
+            "DeltaTime: " +  rectTest.DeltaTime.ToString("F5")
+        );
+    }
+}
--- Assets/Scripts/RectTest.cs
+++ Assets/Scripts/RectTest.cs
@@ -1,32 +1,166 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class RectTest : MonoBehaviour
 {
     public float CellSize = 10.0f;
-    [Space]
-    public Vector2 MassRectPos1 = new Vector2( 2.5f, -2.5f );
-    public Vector2 MassRectSize1 = new Vector2(2.5f, 2.5f);
+    public float MaxCellOffset = 0.5f;
+    [Header("Cell1")]
+    public Vector2 CellPos1 = new Vector2(0,0);
+    public Vector4 MassRect1 = new Vector4( -5, -5, 5, 5 );
     public float MassValue1 = 1.0f;
-    [Space]
-    public Vector2 MassRectPos2 = new Vector2( 2.5f, -2.5f );
-    public Vector2 MassRectSize2 = new Vector2(2.5f, 2.5f);
+    public Vector2 Vel1 = Vector2.zero;
+    [Header("Cell2")]
+    public Vector2 CellPos2 = new Vector2(-10,0);
+    public Vector4 MassRect2 = new Vector4( -5, -5, 5, 5 );
     public float MassValue2 = 1.0f;
+    public Vector2 Vel2 = Vector2.zero;
+
+    public float DeltaTime
+    {
+        get
+        {
+            float maxVel = Mathf.Max(Vel1.magnitude,Vel2.magnitude);
+            if (maxVel > 0)
+            {
+                return CellSize / maxVel * MaxCellOffset;
+            }
+            else
+            {
+                return 1.0f;
+            }
+        }
+    }
+
+    public Vector4 Bounds
+    {
+        get
+        {
+            float xInf = CellPos1.x - CellSize / 2;
+            float yInf = CellPos1.y - CellSize / 2;
+            float xSup = CellPos1.x + CellSize / 2;
+            float ySup = CellPos1.y + CellSize / 2;
+            
+            xInf = Mathf.Min( xInf, CellPos2.x - CellSize / 2);
+            yInf = Mathf.Min( yInf, CellPos2.y - CellSize / 2);
+            xSup = Mathf.Max( xSup, CellPos2.x + CellSize / 2);
+            ySup = Mathf.Max( ySup, CellPos2.y + CellSize / 2);
+            
+            return new Vector4( xInf, yInf, xSup, ySup );
+        }
+    }
+
+    Vector4 Intersect(Vector4 aabb1, Vector4 aabb2)
+    {
+        float xInf1 = aabb1.x;
+        float yInf1 = aabb1.y;
+        float xSup1 = aabb1.z;
+        float ySup1 = aabb1.w;
+        float xInf2 = aabb2.x;
+        float yInf2 = aabb2.y;
+        float xSup2 = aabb2.z;
+        float ySup2 = aabb2.w;
+        
+        return new Vector4
+        (
+            Mathf.Min( Mathf.Max(xInf1,xInf2), xSup2 ),
+            Mathf.Min( Mathf.Max(yInf1,yInf2), ySup2 ),
+            Mathf.Max( Mathf.Min(xSup1,xSup2), xInf2 ),
+            Mathf.Max( Mathf.Min(ySup1,ySup2), yInf2 )
+        );
+    }
 
     void OnDrawGizmos()
     {
-        Gizmos.DrawWireCube( Vector3.zero, new Vector3(CellSize,0,CellSize) );
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireCube( new Vector3(MassRectPos1.x,0.0f,MassRectPos1.y), new Vector3(MassRectSize1.x,0,MassRectSize1.y) );
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireCube( new Vector3(MassRectPos2.x,0.0f,MassRectPos2.y), new Vector3(MassRectSize2.x,0,MassRectSize2.y) );
+        Gizmos.color = Color.gray;
+        Gizmos.DrawWireCube( new Vector3(CellPos1.x,0,CellPos1.y), new Vector3(CellSize,0,CellSize) );
+        Gizmos.DrawWireCube( new Vector3(CellPos2.x,0,CellPos2.y), new Vector3(CellSize,0,CellSize) );
 
-        float totalMassValue = (MassValue1 + MassValue2);
-        Vector2 massRectPos3 = MassRectPos1 * MassValue1 / totalMassValue + MassRectPos2 * MassValue2 / totalMassValue;
-        Vector2 massRectSize3 = MassRectSize1 * MassValue1 / totalMassValue + MassRectSize2 * MassValue2 / totalMassValue;
+        Vector2 ExtentOfMass1 = new Vector2((MassRect1.z - MassRect1.x), (MassRect1.w - MassRect1.y));
+        Vector2 CenterOfMass1 = CellPos1 + new Vector2(MassRect1.x + ExtentOfMass1.x/2, MassRect1.y + ExtentOfMass1.y/2);
         
+        Gizmos.color = Color.Lerp(Color.gray, Color.yellow, 0.66f);
+        Gizmos.DrawWireCube( new Vector3(CenterOfMass1.x,0.0f,CenterOfMass1.y), new Vector3(ExtentOfMass1.x,0,ExtentOfMass1.y) );
+        Gizmos.color = Color.Lerp(Color.gray, Color.red, 0.33f);
+        Gizmos.DrawLine(new Vector3(CellPos1.x, 0, CellPos1.y),new Vector3(CellPos1.x + Vel1.x, 0, CellPos1.y + Vel1.y));
+        Gizmos.DrawSphere(new Vector3(CellPos1.x, 0, CellPos1.y), CellSize / 50);
+        
+        Vector2 ExtentOfMass2 = new Vector2((MassRect2.z - MassRect2.x), (MassRect2.w - MassRect2.y));
+        Vector2 CenterOfMass2 = CellPos2 + new Vector2(MassRect2.x + ExtentOfMass2.x/2, MassRect2.y + ExtentOfMass2.y/2);
+        
+        Gizmos.color = Color.Lerp(Color.gray, Color.green, 0.66f);
+        Gizmos.DrawWireCube( new Vector3(CenterOfMass2.x,0.0f,CenterOfMass2.y), new Vector3(ExtentOfMass2.x,0,ExtentOfMass2.y) );
+        Gizmos.color = Color.Lerp(Color.gray, Color.red, 0.33f);
+        Gizmos.DrawLine(new Vector3(CellPos2.x, 0, CellPos2.y),new Vector3(CellPos2.x + Vel2.x, 0, CellPos2.y + Vel2.y));
+        Gizmos.DrawSphere(new Vector3(CellPos2.x, 0, CellPos2.y), CellSize / 50);
+        
+        // mass rects with offset
+
+        Vector2 MassRectOffset1 = Vel1 * DeltaTime;
+        
+        Gizmos.color = Color.Lerp(Color.gray, Color.yellow, 0.33f);
+        Gizmos.DrawWireCube( new Vector3(CenterOfMass1.x+MassRectOffset1.x,0.0f,CenterOfMass1.y+MassRectOffset1.y), new Vector3(ExtentOfMass1.x,0,ExtentOfMass1.y) );
+        
+        Vector2 MassRectOffset2 = Vel2 * DeltaTime;
+        
+        Gizmos.color = Color.Lerp(Color.gray, Color.green, 0.33f);
+        Gizmos.DrawWireCube( new Vector3(CenterOfMass2.x+MassRectOffset2.x,0.0f,CenterOfMass2.y+MassRectOffset2.y), new Vector3(ExtentOfMass2.x,0,ExtentOfMass2.y) );
+        
+        // intersect <mass rect with offset #1> and <cell #1>
+        
+        Vector4 aabbCell1 = new Vector4( CellPos1.x - CellSize/2, CellPos1.y - CellSize/2, CellPos1.x + CellSize/2, CellPos1.y + CellSize/2 );
+        
+        Vector4 aabbMassRectWidthOffset1 = new Vector4
+        (
+            CenterOfMass1.x+MassRectOffset1.x-ExtentOfMass1.x/2,
+            CenterOfMass1.y+MassRectOffset1.y-ExtentOfMass1.y/2,
+            CenterOfMass1.x+MassRectOffset1.x+ExtentOfMass1.x/2,
+            CenterOfMass1.y+MassRectOffset1.y+ExtentOfMass1.y/2
+        );
+        Vector4 aabbIntersection1 = Intersect(aabbMassRectWidthOffset1, aabbCell1);
+        
+        Gizmos.color = Color.Lerp(Color.gray, Color.yellow, 0.99f);
+        Gizmos.DrawWireCube( 
+            new Vector3(aabbIntersection1.x+(aabbIntersection1.z-aabbIntersection1.x)/2,0.0f,aabbIntersection1.y+(aabbIntersection1.w-aabbIntersection1.y)/2), 
+            new Vector3((aabbIntersection1.z-aabbIntersection1.x),0,(aabbIntersection1.w-aabbIntersection1.y)) 
+        );
+        
+        // intersect <mass rect with offset #2> and <cell #1>
+        
+        Vector4 aabbMassRectWidthOffset2 = new Vector4
+        (
+            CenterOfMass2.x+MassRectOffset2.x-ExtentOfMass2.x/2,
+            CenterOfMass2.y+MassRectOffset2.y-ExtentOfMass2.y/2,
+            CenterOfMass2.x+MassRectOffset2.x+ExtentOfMass2.x/2,
+            CenterOfMass2.y+MassRectOffset2.y+ExtentOfMass2.y/2
+        );
+        Vector4 aabbIntersection2 = Intersect(aabbMassRectWidthOffset2, aabbCell1);
+        
+        Gizmos.color = Color.Lerp(Color.gray, Color.green, 0.99f);
+        Gizmos.DrawWireCube( 
+            new Vector3(aabbIntersection2.x+(aabbIntersection2.z-aabbIntersection2.x)/2,0.0f,aabbIntersection2.y+(aabbIntersection2.w-aabbIntersection2.y)/2), 
+            new Vector3((aabbIntersection2.z-aabbIntersection2.x),0,(aabbIntersection2.w-aabbIntersection2.y)) 
+        );
+        
+        // compute masses for rects
+
+        float cellArea = CellSize * CellSize;
+        float massRectArea1 = (aabbIntersection1.z - aabbIntersection1.x) * (aabbIntersection1.w - aabbIntersection1.y); 
+        float massRectArea2 = (aabbIntersection2.z - aabbIntersection2.x) * (aabbIntersection2.w - aabbIntersection2.y);
+        float mass1 = MassValue1 * massRectArea1 / cellArea;
+        float mass2 = MassValue2 * massRectArea2 / cellArea;
+        
+        // compute result mass rect of cell #1
+
+        float totalMass = (mass1 + mass2);
+        Vector4 aabbResult = aabbIntersection1 * mass1 / totalMass + aabbIntersection2 * mass2 / totalMass;   
+
         Gizmos.color = Color.blue;
-        Gizmos.DrawWireCube( new Vector3(massRectPos3.x,0.0f,massRectPos3.y), new Vector3(massRectSize3.x,0,massRectSize3.y) );
+        Gizmos.DrawWireCube( 
+            new Vector3(aabbResult.x+(aabbResult.z-aabbResult.x)/2,0.0f,aabbResult.y+(aabbResult.w-aabbResult.y)/2), 
+            new Vector3((aabbResult.z-aabbResult.x),0,(aabbResult.w-aabbResult.y)) 
+        );
     }
 }
